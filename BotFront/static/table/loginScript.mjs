3c216ee8a9bfce8e3a7db9c4a50a4b494f9e1a22
Houston1304 <64915948+Houston1304@users.noreply.github.com>
2023-02-21 00:35:41 +0300

    Update loginScript.mjs

--- BotFront/static/table/loginScript.mjs
+++ BotFront/static/table/loginScript.mjs
@@ -4,23 +4,27 @@ let body = 0;
 if (selectTable == "Пользователи, зашедшие в бота") {
   body = 1;
 }
-try {
-  let response = await fetch("/login_admin", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
-  if (!response.ok) {
-    throw Error(response.statusText);
-  } else {
-    console.log(response);
-  }
-  if (!response.ok) {
-    alert("Ошибка");
-    throw Error(response.statusText);
-  } else {
-    console.log(response);
-  }
-} catch {}
+async function sendSwitch() {
+  try {
+    let response = await fetch("/login_admin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    if (!response.ok) {
+      throw Error(response.statusText);
+    } else {
+      console.log(response);
+    }
+    if (!response.ok) {
+      alert("Ошибка");
+      throw Error(response.statusText);
+    } else {
+      console.log(response);
+    }
+  } catch {}
+}
+
+sendSwitch();
